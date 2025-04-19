c3a819e3ec687dddf762377a68b1a79edb263c27
Egor_Granin <epichescktdm@gmail.com>
2025-04-19 21:52:20 +0700

    Done NeuralNet

--- apps/NeuralNetwork/Neural.js
+++ apps/NeuralNetwork/Neural.js
@@ -9,7 +9,6 @@ function multi(matrix,neurons, neurons2) {
     }
 }
 
-
 function activateFunc(values){
     for(let i = 0;i<values.length;i++) {
         values[i] = Math.tanh(values[i])
@@ -29,24 +28,16 @@ function searchMax(values){
 }
 
 function softmax(arr) {
-    // Находим сумму экспонент всех элементов массива
-    const expValues = arr.map(x => Math.exp(x)); // Вычисляем экспоненту для каждого элемента
-    const sumExp = expValues.reduce((a, b) => a + b, 0); // Суммируем все экспоненты
-
-    // Применяем softmax: делим каждую экспоненту на сумму
+    const expValues = arr.map(x => Math.exp(x));
+    const sumExp = expValues.reduce((a, b) => a + b, 0);
     return expValues.map(x => x / sumExp);
 }
 
 function feedForward(data, weights1, weights2, neurons2, neurons3) {
-    let neurons1 = data;
-
-    multi(weights1, neurons1, neurons2);
+    multi(weights1, data, neurons2);
     activateFunc(neurons2);
-
     multi(weights2, neurons2, neurons3);
-
     const output = softmax(neurons3);
-    console.log(output)
     return searchMax(output);
 }
 
@@ -55,16 +46,9 @@ async function Neural(data){
     let weights = await response.json();
     let weights1 = weights.weights_0_1;
     let weights2 = weights.weights_1_2;
-    console.log(data)
-    console.log("weights_0_1:", weights1);
-    console.log("weights_1_2:", weights2);
     let neurons2 = Array(100).fill(0);
     let neurons3 = Array(10).fill(0);
     let S1 = Array(100)
     let S2 = Array(10)
-    // console.log(data)
-    // console.log(weights1);
-    // console.log(weights2);
-    console.log(neurons3);
-    console.log(feedForward(data,weights1,weights2,neurons2,neurons3))
+    return feedForward(data,weights1,weights2,neurons2,neurons3)
 }
