71127d22ee5465f91ac6d9e64b5c8de5d2ba4fa0
Maxim Staricov <Max47911.eng@gmail.com>
2025-04-25 10:06:40 +0700

    Release build. Changed: - Population creation procedure - Chromosome creation procedure - Chromosome is now moved to class - Chromosome drawing function - Visual confirmation that the algorithm has found a global optimum and converged - Parameters regulating the algorithm operation are moved to global
    Removed:
    - Proportional fitness system
    
    Added:
    - Dynamic change in the probability of mutation of children's genes
    - Selection by tournament selection
    - Added 2-opt algorithm: now the chromosome is not a random path, but a random local optimum
    - Protection during algorithm execution: you cannot put a new point until the algorithm converges
    
    Bugs fixed:
    - Crossover bug fixed: parents' genes are now correctly inherited into the children's gene array. Added protection.
    - Fixed a bug where the site would freeze when crossover was incorrect
    - The population does not get stuck in the local optimum found
    - Fixed bugs with dynamic canvas
    - Fixed a bug where the user puts a dot during the execution of the algorithm
    - Fixed a bug when shuffling an array

--- apps/Genetic/script.js
+++ apps/Genetic/script.js
@@ -4,8 +4,11 @@ const startBtn = document.getElementById("startBtn");
 
 const points = [];
 
+var CAN_DRAW = true;
 
 background_canvas.addEventListener("click", function(event) {
+    if(!CAN_DRAW) return;
+
     const rect = background_canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
@@ -19,74 +22,9 @@ background_canvas.addEventListener("click", function(event) {
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.fillStyle = "red";
     ctx.fill();
-
-    // console.log(`Point added at (${x.toFixed(0)}, ${y.toFixed(0)})`);
 });
 
 
 startBtn.addEventListener("click", () => {
     start(dynamic_canvas, points);
 });
-
-
-
-
-// const background_canvas = document.getElementById("backgroundCanvas");
-// const dynamic_canvas = document.getElementById("dynamicCanvas");
-// const startBtn = document.getElementById("startBtn");
-
-// const points = [];
-
-// background_canvas.addEventListener("click", function(event) {
-
-//     const rect = background_canvas.getBoundingClientRect();
-//     const x = event.clientX - rect.left;
-//     const y = event.clientY - rect.top;
-
-//     points.push({x, y});
-
-//     const radius = 5;
-//     const ctx = background_canvas.getContext("2d");
-
-//     ctx.beginPath();
-//     ctx.arc(x, y, radius, 0, Math.PI * 2);
-//     ctx.fillStyle = "red";
-//     ctx.fill();
-    
-//     console.log(`Point added at (${x.toFixed(0)}, ${y.toFixed(0)})`); // Для отладки
-// });
-
-// startBtn.addEventListener("click", () => {
-//     if (points.length < 2) {
-//         alert("Please add at least 2 points first");
-//         return;
-//     }
-//     start(dynamic_canvas, points);
-// });
-
-
-
-
-// // const background_canvas = document.getElementById("backgroundCanvas");
-// // const dynamic_canvas = document.getElementById("dynamicCanvas")
-
-// // const startBtn = document.getElementById("startBtn");
-
-// // const points = [];
-
-// // background_canvas.addEventListener("click", function(event) {
-// //     const rect = background_canvas.getBoundingClientRect();
-// //     const x = event.clientX - rect.left;
-// //     const y = event.clientY - rect.top;
-
-// //     points.push({x, y});
-
-// //     const radius = 5;
-// //     const ctx = background_canvas.getContext("2d");
-// //     ctx.beginPath();
-// //     ctx.arc(x, y, radius, 0, Math.PI * 2);
-// //     ctx.fillStyle = "red";
-// //     ctx.fill();
-// // });
-
-// // startBtn.addEventListener("click", () => start(dynamic_canvas, points));
